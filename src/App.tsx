9d785845bd326516eb8cce450799737d28125c09
caleb-freitas <calebfreitas@tutanota.com>
2022-06-21 19:42:21 -0300

    feat: added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query GetLessons {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-type Lesson = {
-  id: string
-  title: string
-
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
