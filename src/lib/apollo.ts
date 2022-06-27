83574642f145923c171d690998d55cd8235bb2d9
caleb-freitas <calebfreitas@tutanota.com>
2022-06-27 18:03:20 -0300

    chore: used env variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { env } from "./env";
 
 export const apolloClient = new ApolloClient({
-  uri: env.uri,
+  uri: import.meta.env.VITE_API_URI,
+  headers: {
+    'Authentication': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
 })
